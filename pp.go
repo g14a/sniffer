dc91532efea879a81338c016bbc99ff887f270a4
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-11-29 18:58:53 +0530

    Printed IP header using colors.

--- pp.go
+++ pp.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"time"
 
+	"github.com/fatih/color"
+
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
@@ -35,37 +37,53 @@ func main() {
 
 func printPacketInfo(packet gopacket.Packet) {
 	// Let's see if the packet is an ethernet packet
-	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
-	if ethernetLayer != nil {
-		fmt.Println("Ethernet layer detected.")
-		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+	// ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	// if ethernetLayer != nil {
+	// 	fmt.Println("Ethernet layer detected.")
+	// 	ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
 
-		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
-		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
-		// Ethernet type is typically IPv4 but could be ARP or other
-		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
-		fmt.Println()
-	}
+	// 	fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+	// 	fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+	// 	// Ethernet type is typically IPv4 but could be ARP or other
+	// 	fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+	// 	fmt.Println()
+	// }
 
 	// Let's see if the packet is IP (even though the ether type told us)
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer != nil {
-		fmt.Println("IPv4 layer detected.")
 		ip, _ := ipLayer.(*layers.IPv4)
 
-		fmt.Println("IP Version : ", ip.Version)
-		fmt.Println("IHL: ", ip.IHL)
-		fmt.Println("TOS: ", ip.TOS)
-		fmt.Println("Length: ", ip.Length)
-		fmt.Println("Id: ", ip.Id)
-		fmt.Println("TTL: ", ip.TTL)
-		fmt.Println("Checksum: ", ip.Checksum)
-		fmt.Println("SRC IP: ", ip.SrcIP)
-		fmt.Println("DstIP: ", ip.DstIP)
-		fmt.Println("Options: ", ip.Options)
-		fmt.Println("Fragment Offset: ", ip.FragOffset)
-		fmt.Println("Flags: ", ip.Flags)
+		side := color.New(color.FgCyan)
+		value := color.New(color.FgYellow)
 
+		side.Print("IP Version: ")
+		value.Print(ip.Version, " | ")
+		side.Print("IHL: ")
+		value.Print(ip.IHL, "   | ")
+		side.Print("TOS: ")
+		value.Print(ip.TOS, " | ")
+		side.Print("Length: ")
+		value.Print(ip.Length, " 	 |\n")
+		side.Print("     ID: ")
+		value.Print(ip.Id)
+		value.Print("           | ")
+		side.Print("Flags: ")
+		value.Print(ip.Flags, "  | ")
+		side.Print("OFF: ")
+		value.Print(ip.FragOffset, " 	   | ")
 		fmt.Println()
+		side.Print("TTL: ")
+		value.Print(ip.TTL, "       | ")
+		side.Print("Pro: ")
+		value.Print(ip.Protocol, "  | ")
+		side.Print("	CHS: ")
+		value.Print(ip.Checksum, "   	   | \n")
+		side.Print("		Src: ")
+		value.Print(ip.SrcIP, "	    	   | \n")
+		side.Print("	        Dst: ")
+		value.Print(ip.DstIP, " 		   |  ")
+
+		fmt.Println("\n\n")
 	}
 }
