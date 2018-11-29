78c3bb5df8b312840c7f16025813eb96c3827802
gowtham-munukutla <gowtham.m81197@gmail.com>
2018-11-29 17:53:08 +0530

    Raw printed details of Ethernet and IPv4.

--- pp.go
+++ pp.go
@@ -2,44 +2,70 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"time"
 
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
 )
 
 var (
-	deviceName  string = "wlp2s0"
-	snapshotLen uint32 = 1024
+	device      string = "wlp2s0"
+	snapshotLen int32  = 1024
 	promiscuous bool   = false
 	err         error
-	timeout     time.Duration = -1 * time.Second
+	timeout     time.Duration = 30 * time.Second
 	handle      *pcap.Handle
-	packetCount int = 0
 )
 
 func main() {
-	// Open output pcap file and write header
-
-	// Open the device for capturing
-	handle, err = pcap.OpenLive(deviceName, int32(snapshotLen), promiscuous, timeout)
+	// Open device
+	handle, err = pcap.OpenLive(device, snapshotLen, promiscuous, timeout)
 	if err != nil {
-		fmt.Printf("Error opening device %s: %v", deviceName, err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	defer handle.Close()
 
-	// Start processing packets
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
-		// Process packet here
-		fmt.Println(packet.String())
-		packetCount++
+		printPacketInfo(packet)
+	}
+}
 
-		// Only capture 100 and then stop
-		if packetCount > 10 {
-			break
-		}
+func printPacketInfo(packet gopacket.Packet) {
+	// Let's see if the packet is an ethernet packet
+	ethernetLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethernetLayer != nil {
+		fmt.Println("Ethernet layer detected.")
+		ethernetPacket, _ := ethernetLayer.(*layers.Ethernet)
+
+		fmt.Println("Source MAC: ", ethernetPacket.SrcMAC)
+		fmt.Println("Destination MAC: ", ethernetPacket.DstMAC)
+		// Ethernet type is typically IPv4 but could be ARP or other
+		fmt.Println("Ethernet type: ", ethernetPacket.EthernetType)
+		fmt.Println()
+	}
+
+	// Let's see if the packet is IP (even though the ether type told us)
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer != nil {
+		fmt.Println("IPv4 layer detected.")
+		ip, _ := ipLayer.(*layers.IPv4)
+
+		fmt.Println("IP Version : ", ip.Version)
+		fmt.Println("IHL: ", ip.IHL)
+		fmt.Println("TOS: ", ip.TOS)
+		fmt.Println("Length: ", ip.Length)
+		fmt.Println("Id: ", ip.Id)
+		fmt.Println("TTL: ", ip.TTL)
+		fmt.Println("Checksum: ", ip.Checksum)
+		fmt.Println("SRC IP: ", ip.SrcIP)
+		fmt.Println("DstIP: ", ip.DstIP)
+		fmt.Println("Options: ", ip.Options)
+		fmt.Println("Fragment Offset: ", ip.FragOffset)
+		fmt.Println("Flags: ", ip.Flags)
+
+		fmt.Println()
 	}
 }
